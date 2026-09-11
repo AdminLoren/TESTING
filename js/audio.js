a754ef07084cc836e2c81b49e92b5111d6f902fa
AdminLoren <lomicarlos2016@gmail.com>
2026-09-11 22:14:16 +0800

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -67,7 +67,7 @@ COTA.audio = (function () {
     if (muted) return;
     try {
       const sfx = new Audio(`assets/audio/${fileName}`);
-      sfx.volume = 0.8;
+      sfx.volume = 0.2;
       sfx.play().catch(() => {});
     } catch (err) {
       /* ignore missing sfx */
